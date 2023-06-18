2112d72be4569aa0fe6ba867ed96ee30365d0c6f
Shirahamah <hiropi1224s@gmail.com>
2023-06-18 19:10:26 +0900

    💄 曜日ごとのページ作成

---
+++ src/app/(withoutFooter)/[dayOfWeek]/page.tsx
@@ -0,0 +1,35 @@
+import { fetchShopListForDayOfWeek } from '@/app/_components/_actions/fetch-shop-list';
+import { ShopList } from '@/app/_components/shop-list';
+
+export default async function DayOfWeek({
+  params,
+}: {
+  params: { dayOfWeek: string };
+}) {
+  const shopList = await fetchShopListForDayOfWeek(params.dayOfWeek);
+
+  return (
+    <main className='flex min-h-screen flex-col items-center'>
+      <ShopList shopList={shopList} dayOfWeek={params.dayOfWeek} />
+    </main>
+  );
+}
+
+export async function generateStaticParams() {
+  const res = await fetch(
+    `${process.env.url}/rest/v1/shop?select=business_day`,
+    {
+      headers: new Headers({
+        apikey: process.env.apikey as string,
+        Authorization: process.env.authorization as string,
+      }),
+      next: { revalidate: 86400 },
+    }
+  );
+
+  const dayOfWeeks: { business_day: string }[] = await res.json();
+
+  return dayOfWeeks.map((dayOfWeek) => ({
+    dayOfWeek: dayOfWeek.business_day,
+  }));
+}
---
+++ src/app/_components/_actions/fetch-shop-list.ts
@@ -0,0 +1,19 @@
+import 'server-only';
+import { Shop } from '@/app/_types/shop';
+
+export const fetchShopListForDayOfWeek = async (dayOfWeek: string) => {
+  const res = await fetch(
+    `${process.env.url}/rest/v1/shop?business_day=eq.${dayOfWeek}&select=*`,
+    {
+      headers: new Headers({
+        apikey: process.env.apikey as string,
+        Authorization: process.env.authorization as string,
+      }),
+      next: { revalidate: 86400 },
+    }
+  );
+
+  const shopList: Shop[] = await res.json();
+
+  return shopList;
+};
