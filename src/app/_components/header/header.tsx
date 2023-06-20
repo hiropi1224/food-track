c549e8cceb99606c5e0a6103420e7a6edf041ea8
Shirahamah <hiropi1224s@gmail.com>
2023-06-21 00:09:07 +0900

    ♻️ headerにLink追加

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -1,11 +1,16 @@
 import { IconTruckDelivery } from '@tabler/icons-react';
+import Link from 'next/link';
 import { FC } from 'react';
 
 export const Header: FC = () => {
   return (
-    <header className='flex items-center gap-2 border-b-2 border-mauve-4'>
-      <IconTruckDelivery size={36} />
-      <h1 className='text-2xl sm:text-4xl'>FoodTracker</h1>
+    <header className='border-b-2 border-mauve-4'>
+      <Link href='/'>
+        <div className='flex items-center gap-2'>
+          <IconTruckDelivery size={36} />
+          <h1 className='text-2xl sm:text-4xl'>FoodTracker</h1>
+        </div>
+      </Link>
     </header>
   );
 };
