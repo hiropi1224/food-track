8bfa22c74aa19624ed1145ea3207a24759f0ad16
Shirahamah <hiropi1224s@gmail.com>
2023-06-23 00:22:18 +0900

    💄 layoutのstyle修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,8 +7,8 @@ export default async function Top() {
   const ids = await fetchShopId();
 
   return (
-    <main className='flex min-h-screen flex-col items-center'>
-      <div className='grid  gap-4 sm:grid-cols-2 md:grid-cols-4'>
+    <main className='flex flex-col items-center'>
+      <div className='grid  gap-4 sm:grid-cols-2'>
         {ids.map((id) => (
           <Link key={id.id} href={`/photos/${id.id}`}>
             <Image src={`/${id.id}.png`} alt='' height={200} width={320} />
