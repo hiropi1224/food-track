ed482fa99608d71cb40ac8ee11c902504a3ef260
Shirahamah <hiropi1224s@gmail.com>
2023-06-25 17:13:44 +0900

    💄 redirect追加

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,8 @@
-import Image from 'next/image';
+import { redirect } from 'next/navigation';
 
-import { fetchShopId } from '@/app/_actions/fetch-shop-list';
+const day = new Date().getDay();
+const dayOfWeek = day === 0 || day === 7 ? '1' : String(day);
 
 export default async function Top() {
-  const ids = await fetchShopId();
-
-  return (
-    <main className='flex flex-col items-center'>
-      <div className='grid  gap-4 sm:grid-cols-2'>
-        {ids.map((id) => (
-          <Image
-            key={id.id}
-            src={`/shop/${id.id}.png`}
-            alt=''
-            height={320}
-            width={320}
-            className='aspect-square'
-          />
-        ))}
-      </div>
-    </main>
-  );
+  redirect(`/${dayOfWeek}`);
 }
