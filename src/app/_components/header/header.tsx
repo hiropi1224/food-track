6597a03809481bcd7e0f627add0900ece4317479
Shirahamah <hiropi1224s@gmail.com>
2023-06-25 17:09:24 +0900

    👔  headerの遷移先修正

--- src/app/_components/header/header.tsx
+++ src/app/_components/header/header.tsx
@@ -2,10 +2,13 @@ import { IconTruckDelivery } from '@tabler/icons-react';
 import Link from 'next/link';
 import { FC } from 'react';
 
+const day = new Date().getDay();
+const dayOfWeek = day === 0 || day === 7 ? '1' : String(day);
+
 export const Header: FC = () => {
   return (
     <header className='mb-2 border-b-2 border-mauve-4'>
-      <Link href='/'>
+      <Link href={`/${dayOfWeek}`}>
         <div className='flex items-center gap-2'>
           <IconTruckDelivery size={36} />
           <h1 className='text-2xl sm:text-4xl'>FoodTracker</h1>
