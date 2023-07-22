2b84b3ed9f0b41c1ef562bb1dc81f9e9a86df186
Shirahamah <hiropi1224s@gmail.com>
2023-07-22 15:37:29 +0900

    ⚡️ #6 dayOfWeekのlayoutにredirect処理を追加

--- src/app/[dayOfWeek]/layout.tsx
+++ src/app/[dayOfWeek]/layout.tsx
@@ -1,7 +1,27 @@
+import { redirect } from 'next/navigation';
+
 export default async function Layout({
+  params,
   children,
 }: {
+  params: { dayOfWeek: string };
   children: React.ReactNode;
 }) {
+  if (!isBusinessDay(params.dayOfWeek)) {
+    const day = new Date().getDay().toString();
+    // 土日の場合は月曜を表示
+    const dayOfWeek = day === '0' || day === '6' ? '1' : day;
+    redirect(`/${dayOfWeek}`);
+  }
+
   return <main>{children}</main>;
 }
+
+/**
+ * 平日かどうかを判定する
+ * @param dayOfWeek
+ * @returns boolean
+ */
+const isBusinessDay = (dayOfWeek: string) => {
+  return Number(dayOfWeek) >= 1 && Number(dayOfWeek) <= 5;
+};
